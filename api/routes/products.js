c077d7da6b25b923f393583ed55b16f79e3fde6e
PreranaPandit <preranapandit07@gmail.com>
2020-01-08 19:35:39 +0545

    Parsing the body

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/',(req, res, next) => {
 });
 
 router.post('/',(req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
     res.status(200).json({
-        message: 'Handling POST request to /products'
+        message: 'Handling POST request to /products',
+        createdProduct: product
     });
 });
 
