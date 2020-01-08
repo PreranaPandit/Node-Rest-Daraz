7156be3cbcb889a59884bdf5e43e52a90263738d
PreranaPandit <preranapandit07@gmail.com>
2020-01-08 18:14:05 +0545

    changes implemented

--- api/routes/products.js
+++ api/routes/products.js
@@ -29,5 +29,16 @@ router.get('/:productId',(req, res,next) => {
       }
 });
 
+router.patch('/:productId',(req, res,next) => {
+  res.status(200).json({
+      message: 'Updated product'
+  });
+});
+
+router.delete('/:productId',(req, res,next) => {
+    res.status(200).json({
+        message: 'deleted product'
+    });
+  });
 
 module.exports = router;
