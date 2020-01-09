becee493d7537a3ec5b4ee4f356e2dd0b7697d40
PreranaPandit <preranapandit07@gmail.com>
2020-01-09 11:50:50 +0545

    Products model added

---
+++ models/products.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
